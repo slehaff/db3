6afbfb9fd729657b6924520553213f6e327a4f32
slehaff <samir@lehaff.dk>
2019-08-16 15:34:17 +0200

    sa

---
+++ calibrate/testing.py
@@ -0,0 +1,15 @@
+import time
+import numpy as np
+
+
+t = time.process_time()
+wrap = np.arctan2(1.7320508 * 2.7, 3.456)
+wrap = np.arctan2(1.7320508 * 2.7, 3.456)
+wrap = np.arctan2(1.7320508 * 2.7, 3.456)
+a = 34.5 * 333.89
+a = 34.5 * 333.89
+a = 34.5 * 333.89
+a = 34.5 * 333.89
+elapsed_time = time.process_time() - t
+print(elapsed_time)
+
